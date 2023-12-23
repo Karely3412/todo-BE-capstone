17dd08e013c92b99a499cff8b69129c9a244d6f4
Karely3412 <karely@devpipeline.com>
2023-12-23 10:45:17 -0700

    inital commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request
+import psycopg2 
+import os
+
+from db import * 
+
+app = Flask(__name__)
+
+flask_host = os.environ.get("FLASK_HOST")
+flask_port = os.environ.get("FLASK_POST")
+
+database_scheme = os.environ.get("DATABASE_SCHEME")
+database_user = os.environ.get("DATABASE_USER")
+database_address = os.environ.get("DATABASE_ADDRESS")
+database_port = os.environ.get("DATABASE_PORT")
+database_name = os.environ.get("DATABASE_NAME")
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f"{database_scheme}{database_user}@{database_address}:{database_port}/{database_name}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8086', debug=True)
---
+++ db.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy.dialects.postgres import 
+
+
+__all__ = ('db', 'init_db')
+
+db = SQLAlchemy()
+
+def init_db(app=None, db=None):
+    if isinstance(app, Flask) and isinstance(db, SQLAlchemy):
+        db.init.app(app)
+
