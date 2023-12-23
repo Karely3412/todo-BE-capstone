c8e8670ca1a176c2c98e1ab90e9b50fba40cad49
Karely3412 <karely@devpipeline.com>
2023-12-23 13:11:47 -0700

    created users checklits xref table

---
+++ models/users_check_lists_xref.py
@@ -0,0 +1,9 @@
+from db import db 
+
+users_check_list_xref = db.Table(
+    "UsersCheckListsXref",
+    db.Model.metadata,
+    db.Column("user_id", db.ForeignKey('Users.user_id'), primary_key=True)
+    db.Column("check_list_id", db.ForeignKey('CheckLists.check_list_id'), primary_key=True)
+)
+
