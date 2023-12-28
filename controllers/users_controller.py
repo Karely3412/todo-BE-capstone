c450e10625e75b1fd1b54c8baacdcd44b4d805b6
Karely3412 <karely@devpipeline.com>
2023-12-27 23:05:14 -0700

    created user update function

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -28,4 +28,25 @@ def user_get_by_id(req, user_id):
     user_query = db.session.query(Users).filter(Users.user_id == user_id).first()
 
     return jsonify({"message ": "user found", "user": user_schema.dump(user_query)})
+
+
+def user_update(req, user_id):
+    post_data = req.form if req.form else req.json
+    user_query = db.session.query(Users).filter(Users.user_id == user_id).first()
+
+    populate_object(user_query, post_data)
+
+    db.session.commit()
+    return jsonify({"message ": "user updated", "user": user_schema.dump(user_query)})
+
+    
+
+
+
+def user_delete(req, user_id):
+    user_query = db.session.query(Users).filter(Users.user_id == user_id).first()
+    
+    db.session.delete(user_query)
+    db.session.commit()
+    return jsonify({"message ": "user successfully deleted"})
     
