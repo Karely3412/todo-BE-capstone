63cf98fb4fa74bb41eaec3d60bdf868b625af161
Karely3412 <karely@devpipeline.com>
2023-12-27 23:25:03 -0700

    finished up with all the user controllers without guard clauses

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -21,13 +21,13 @@ def user_add(req):
 def users_get_all(req):
     users_query = db.session.query(Users).all()
 
-    return jsonify({"message ": "users found", "users": users_schema.dump(users_query)})
+    return jsonify({"message": "users found", "users": users_schema.dump(users_query)})
 
 
 def user_get_by_id(req, user_id):
     user_query = db.session.query(Users).filter(Users.user_id == user_id).first()
 
-    return jsonify({"message ": "user found", "user": user_schema.dump(user_query)})
+    return jsonify({"message": "user found", "user": user_schema.dump(user_query)})
 
 
 def user_update(req, user_id):
@@ -37,7 +37,22 @@ def user_update(req, user_id):
     populate_object(user_query, post_data)
 
     db.session.commit()
-    return jsonify({"message ": "user updated", "user": user_schema.dump(user_query)})
+    return jsonify({"message": "user updated", "user": user_schema.dump(user_query)})
+
+
+def user_activity(req, user_id):
+    user_query = db.session.query(Users).filter(Users.user_id == user_id).first()
+
+    user_query.active = not user_query.active
+    db.session.commit()
+
+    if user_query.active:
+        return jsonify({"message": "user activated", "user": user_schema.dump(user_query)})
+    else:    
+        return jsonify({"message": "user deactivated", "user": user_schema.dump(user_query)})
+    
+
+
 
     
 
