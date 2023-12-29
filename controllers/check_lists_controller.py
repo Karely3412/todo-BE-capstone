1bdda8f752924999d30d8a9a4a6bbbb08fff4ae0
Karely3412 <karely@devpipeline.com>
2023-12-29 12:12:46 -0700

    updated authenticate.py

--- controllers/check_lists_controller.py
+++ controllers/check_lists_controller.py
@@ -2,6 +2,7 @@ from flask import jsonify, request
 from db import db
 
 from models.check_lists import CheckLists, check_lists_schema, check_list_schema
+from models.users import  Users, user_schema, users_schema
 from util.reflecton import populate_object
 
 
@@ -14,25 +15,25 @@ def check_list_add(req):
     db.session.add(new_check_list)
     db.session.commit()
 
-    return jsonify({"message": "check list successfully created", "check_list": check_list_schema.dump(new_check_list)})
+    return jsonify({"message": "checklist successfully created", "check_list": check_list_schema.dump(new_check_list)})
 
 
 def check_lists_get_all(req):
     check_lists_query = db.session.query(CheckLists).all()
 
-    return jsonify({"message": "check lists found", "check_lists": check_lists_schema.dump(check_lists_query) })
+    return jsonify({"message": "checklists found", "check_lists": check_lists_schema.dump(check_lists_query) })
 
 
 def check_list_get_by_id(req, check_list_id):
     check_list_query = db.session.query(CheckLists).filter(CheckLists.check_list_id == check_list_id).first()
 
-    return jsonify({"message": "check list found", "check_list": check_list_schema.dump(check_list_query) })
+    return jsonify({"message": "checklist found", "check_list": check_list_schema.dump(check_list_query) })
 
 
-def check_list_get_by_user_id(req, user_id):
-    check_list_query = db.session.query(CheckLists).filter(CheckLists.check_list_id == user_id).first()
+def check_lists_get_by_user_id(req, user_id):
+    check_list_query = db.session.query(CheckLists).join(Users).first()
 
-    return jsonify({"message": "check list found", "check_list": check_list_schema.dump(check_list_query) })
+    return jsonify({"message": "checklist(s) found", "check_lists": check_lists_schema.dump(check_list_query) })
 
 
 def check_list_delete(req, check_list_id):
@@ -42,7 +43,7 @@ def check_list_delete(req, check_list_id):
     db.session.commit()
     
 
-    return jsonify({"message": "check list deleted"})
+    return jsonify({"message": "checklist deleted"})
     
     
 
---
+++ lib/authenticate.py
@@ -0,0 +1,35 @@
+import functools
+from flask import jsonify
+from datetime import datetime
+
+from db import db
+from models.auth_tokens import AuthTokens
+from util.validate_uuid4 import validate_uuid4
+
+def validate_token(args):
+    auth_token = args.headers['auth']
+
+    if not auth_token or not validate_uuid4(auth_token):
+        return False
+
+    existing_token = db.session.query(AuthTokens).filter(AuthTokens.auth_token == auth_token).first()
+
+    if existing_token:
+        if existing_token.expiration > datetime.now():
+            return existing_token
+    else:
+        return False
+
+def fail_response():
+    return jsonify({"message": "authentication required"}), 401
+
+def authenticate(func):
+    @functools.wraps(func)
+    def wrapper_auth_return(*args, **kwargs):
+        auth_info = validate_token(kwargs['request'])
+
+        if auth_info:
+            return func(*args, **kwargs)
+        else:
+            return fail_response()
+    return wrapper_auth_return
